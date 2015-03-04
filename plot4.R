0d49830ca948efb873dc3e1537cc2bb95777c510
bitsofbits <tim@bitsofbits.com>
2015-03-04 10:39:01 -0700

    Add R files and plots for project 1

---
+++ plot4.R
@@ -0,0 +1,43 @@
+# The following preamable is shared between the plots. Normally I'd consider factoring this out and 
+# sharing it between plots, but given the format of the assignment I don't want to cause confusion
+
+projData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE)
+# Repurpose Time field as a Date and Time field since strptime gives as a Date/Time object anyway
+# The resulting times are in the local timezone, but we treat them as naive times and ignore that.
+dtFormat <- "%d/%m/%Y %H:%M:%S"
+projData$Time <- strptime(paste(projData$Date, projData$Time), format=dtFormat)
+
+startTime = strptime("01/02/2007 00:00:00", format=dtFormat)
+stopTime = strptime("02/02/2007 23:59:59", format=dtFormat)
+subset = projData[(projData$Time >= startTime) & (projData$Time <= stopTime),]
+
+subset$Global_active_power = as.numeric(subset$Global_active_power)
+subset$Sub_metering_1 = as.numeric(subset$Sub_metering_1)
+subset$Sub_metering_2 = as.numeric(subset$Sub_metering_2)
+subset$Sub_metering_3 = as.numeric(subset$Sub_metering_3)
+subset$Voltage = as.numeric(subset$Voltage)
+subset$Global_reactive_power = as.numeric(subset$Global_reactive_power)
+
+# Plot specific code.
+png("plot4.png")
+
+
+orig_par <- par(mfrow=c(2,2))
+
+plot(subset$Time, subset$Global_active_power, ylab="Global Active Power", main="", xlab="", type="l")
+
+plot(subset$Time, subset$Voltage, ylab="Voltage", main="", xlab="datetime", type="l")
+
+plot(subset$Time, subset$Sub_metering_1, ylab="Energy sub metering", main="", xlab="", type="l")
+points(subset$Time, subset$Sub_metering_2, type="l", col="red")
+points(subset$Time, subset$Sub_metering_3, type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"), lty=1, bty="n")
+
+plot(subset$Time, subset$Global_reactive_power, ylab="Global_reactive_power", main="", 
+     xlab="datetime", type="l")
+
+par(orig_par)
+
+dev.off()
+
